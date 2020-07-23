5a5a146adedc931c3584732d48aaf5df09e68fe1
John Robles <john.robles1@upr.edu>
2020-07-23 17:28:05 -0400

    Event class Done

---
+++ models/event.js
@@ -0,0 +1,14 @@
+class Event {
+    constructor(id, title, description, date, time, name, image, location) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.date = date;
+        this.time = time;
+        this.name = name;
+        this.image = image;
+        this.location = location;
+    }
+}
+
+export default Event;
