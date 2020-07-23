8710ce3b06aba7306655c7f7de1c1e76dba2aac4
John Robles <john.robles1@upr.edu>
2020-07-23 18:54:13 -0400

    Adding dependencies, color and EventNavigator Done

---
+++ navigation/EventNavigator.js
@@ -0,0 +1,25 @@
+import { Platform } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import CreateEventScreen from '../screens/CreateEventScreen';
+import EventDetailScreen from '../screens/EventDetailScreen';
+import MainPageScreen from '../screens/MainPageScreen';
+import Colors from '../constants/Colors';
+
+const EventNavigator = createStackNavigator({
+    CreateEvent: CreateEventScreen,
+    EventDetail: EventDetailScreen,
+    MainPage: MainPageScreen
+}, {
+    defaultNavigationOptions: {
+        headerStyle: {
+            backgroundColor: Platform.OS === 'android' ? Colors.header : ''
+        },
+        headerTintColor: Platform.OS === 'android' ? 'white' : Colors.header
+    }
+});
+
+const AppContainer = createAppContainer(EventNavigator);
+
+export default AppContainer;
