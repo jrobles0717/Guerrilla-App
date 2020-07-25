6992ffd97a5b74bc6891500b83b986b032d7866f
John Robles <john.robles1@upr.edu>
2020-07-25 13:43:34 -0400

    fix error loading to create event

--- App.js
+++ App.js
@@ -5,9 +5,28 @@ import { Provider } from 'react-redux';
 import ReduxThunk from 'redux-thunk';
 
 import EventNavigator from './navigation/EventNavigator';
+import eventsReducer from './store/reducers/events';
+import { init } from './helpers/db';
+
+init()
+  .then(() => {
+    console.log('Initialized database');
+  })
+  .catch(err => {
+    console.log('Initializing db failed.');
+    console.log(err);
+  });
+
+const rootReducer = combineReducers({
+  events: eventsReducer
+});
+
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
 export default function App() {
   return (
-    <EventNavigator />
+    <Provider store={store}>
+      <EventNavigator />
+    </Provider>
   );
 }
