32b5582d55cf03debc72a839a44c276eb711aaf1
John Robles <john.robles1@upr.edu>
2020-07-26 22:30:29 -0400

    update to real props, still undone

--- components/EventItem.js
+++ components/EventItem.js
@@ -4,23 +4,75 @@ import Colors from '../constants/Colors';
 
 const EventItem = props => {
     return (
-        <TouchableOpacity>
-            <Image />
-            <View>
-                <Text>Title Event</Text>
-                <Text>Start: Jul 20, 2020</Text>
-                <Text>7:00 PM</Text>
+        <TouchableOpacity onPress={props.onSelect} style={styles.eventItem}>
+            <Image style={styles.image} source={{ uri: props.image }}/>
+            <View style={styles.infoContainer}>
+                <Text style={styles.title}>{props.title}</Text>
+                <Text style={styles.date}>Start: {props.date}</Text>
+                <Text style={styles.time}>{props.time}</Text>
             </View>
-            <View>
-                <Text>Participants</Text>
-                <Text>1</Text>
+            <View style={styles.confirmedContainer}>
+                <Text style={styles.participants}>Participants</Text>
+                <Text style={styles.numberConfirmed}>1</Text>
             </View>
         </TouchableOpacity>
     );
 };
 
 const styles = StyleSheet.create({
-
+    eventItem: {
+        borderBottomColor: '#ccc',
+        borderBottomWidth: 1,
+        paddingVertical: 15,
+        paddingHorizontal: 30,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    image: {
+        width: 70,
+        height: 70,
+        borderRadius: 35,
+        backgroundColor: '#ccc',
+        borderColor: Colors.primary,
+        borderWidth: 1
+    },
+    infoContainer: {
+        marginLeft: 25,
+        width: 250,
+        justifyContent: 'center',
+        alignItems: 'flex-start' 
+    },
+    title: {
+        color: 'black',
+        fontSize: 18,
+        marginBottom: 5
+    },
+    date: {
+        color: 'black',
+        fontSize: 16
+    },
+    time: {
+        color: 'black',
+        fontSize: 16
+    },
+    confirmedContainer: {
+        marginLeft: 25,
+        width: 250,
+        justifyContent: 'center',
+        alignItems: 'flex-start'
+    },
+    participants: {
+        color: 'black',
+        fontSize: 14
+    },
+    numberConfirmed: {
+        width: 60,
+        height: 60,
+        borderRadius: 35,
+        backgroundColor: '#ccc',
+        borderColor: Colors.header,
+        borderWidth: 1
+    }
 });
 
 export default EventItem;
